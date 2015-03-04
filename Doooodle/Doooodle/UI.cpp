8948f04d4cccdf0eaa8d6024f6d7d9576207278c
cindyfu421 <fuxin421@Gmail.com>
2015-03-04 18:55:15 +0800

    Basic UI functions

---
+++ Doooodle/Doooodle/UI.cpp
@@ -0,0 +1,25 @@
+#include "UI.h"
+
+const string UI::MESSAGE_ADD = "added to calendar successfully";
+const string UI::MESSAGE_DEFAULT = "Enter Command:";
+const string UI::MESSAGE_WELCOME = "*************************\n\
+*        Doooodle       *\n*************************\n";
+
+UI::UI(void) {
+}
+UI::~UI(void) {
+}
+void UI::readMessage(){
+	Logic logic;
+	string command;
+	string message;
+	cout << MESSAGE_WELCOME<<MESSAGE_DEFAULT;
+	getline(cin, command);
+	cout << endl;
+	message=logic.receiveCommand(command);
+	printToUser(message);
+}
+
+void UI::printToUser(string message){
+	cout << message;
+}
