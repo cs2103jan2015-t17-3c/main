6bd7dcd92ec389bbfe94c2ea094b064169ff7b78
MitchTan <mitchell_tan@hotmail.com>
2015-03-26 16:24:24 +0800

    Minor edit to Logic

--- Doooodle/Doooodle/EditSearchTask.cpp
+++ Doooodle/Doooodle/EditSearchTask.cpp
@@ -8,7 +8,7 @@ EditSearchTask::~EditSearchTask(void) {
 }
 
 string EditSearchTask::loadTask(int index, string stringToEdit, boost::gregorian::date startDate, boost::gregorian::date endDate, boost::posix_time::ptime startTime, boost::posix_time::ptime endTime, Storage& storage){
-	displayMessage = storage.editTask(index, stringToEdit, startDate, endDate, startTime, endTime);
+	displayMessage = storage.editSearchTask(index, stringToEdit, startDate, endDate, startTime, endTime);
 	storage.sortStorage();
 	storage.writeToFile();
 	assert(!(displayMessage.empty()));
