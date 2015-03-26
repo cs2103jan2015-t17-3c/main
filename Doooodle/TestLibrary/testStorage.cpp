dc1fc55b0a32a88db21c0f9787588eeb0b3cc9d1
Funtomxu <yanxujq@gmail.com>
2015-03-26 14:26:12 +0800

    storage test update

--- Doooodle/TestLibrary/testStorage.cpp
+++ Doooodle/TestLibrary/testStorage.cpp
@@ -14,7 +14,7 @@ namespace TestLibrary
 			Storage storage;
 			boost::posix_time::ptime t(boost::gregorian::date(boost::gregorian::day_clock::local_day()), boost::posix_time::hours(22) + boost::posix_time::minutes(0));
 			string A = "task";
-			string expected="Deadline task: task                      by 2015-Mar-25 22:00 successfully added.\n";
+			string expected="Deadline task: task by 2015-Mar-25 22:00 successfully added.\n";
 			string actual = storage.addDeadlineTask(A, d, t);
 			Assert::AreEqual(expected, actual);
 			
