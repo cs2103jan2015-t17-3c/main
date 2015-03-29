24255e46aa5308c8789fa2b8fad05dc807c9c44e
Lordwang <wyf0423@gmail.com>
2015-03-29 17:01:12 +0800

    edit UI display

--- Doooodle/Doooodle/UI.cpp
+++ Doooodle/Doooodle/UI.cpp
@@ -1,11 +1,19 @@
 #include "UI.h"
 
-const string UI::MESSAGE_DEFAULT = "Commands available:add, delete, edit, search\nEnter Command:";
+const string UI::MESSAGE_DEFAULT = "Commands available:add, delete, edit, search, undo\nEnter Command:";
+const string UI::MESSAGE_DEFAULT_CHECK = "Commands available:completed, reschedule\nEnter Command:";
 const string UI::MESSAGE_WELCOME = "*************************\n\
 *        Doooodle       *\n*************************\n";
-const string UI::MESSAGE_TOPTEN = "Below is the list of top ten items in your scheduler for now\n\n";
-const string UI::MESSAGE_FLOAT = "Below is the tasks without specific date or time\n";
+const string UI::MESSAGE_TOPTEN = "Below is the list of top ten items in your scheduler\n\n";
+const string UI::MESSAGE_FLOAT = "Below are the tasks without specific date or time\n";
 const string UI::MESSAGE_SEARCH = "Below is the search result\n";
+const string UI::MESSAGE_CHECK = "Have you completed the following task(s)?\n";
+const string UI::MESSAGE_CHECK_ADD = "Before add, have you completed the following task(s)?\n";
+const string UI::MESSAGE_CHECK_DELETE = "Before delete, have you completed the following task(s)?\n";
+const string UI::MESSAGE_CHECK_SEARCH = "Before search, have you completed the following task(s)?\n";
+const string UI::MESSAGE_CHECK_EDIT = "Before edit, have you completed the following task(s)?\n";
+const string UI::MESSAGE_CHECK_ARCHIVE = "Before showing archive, have you completed the following task(s)?\n";
+
 const int UI::NUMBER_OF_TOTAL_LINES = 12;
 const int UI::NUMBER_OF_SEARCH_LINES = 27;
 UI::UI(void) {
@@ -21,23 +29,19 @@ void UI::readMessage(){
 	cout << MESSAGE_WELCOME;
 
 	while (true){
-		//assertion added by cindy
 		cout << "Today: "<<today << endl << endl;
 		assert(command != "");
-		if (logic.isSearch(command)){
+		if (logic.getCommandType(command) == "search"){
 			cout << MESSAGE_SEARCH << endl;
 			displayMessage = logic.displaySearchResults(command);
 			printVectorToUser(displayMessage);
-			printSearchLine(displayMessage.size());
-		}else{
+		} else{
 			cout << MESSAGE_TOPTEN;
 			displayMessage = logic.displayTopTen();
 			printVectorToUser(displayMessage);
-			printEmptyLine(displayMessage.size());
 			cout << MESSAGE_FLOAT;
 			displayMessage = logic.displayFloatingTask();
 			printVectorToUser(displayMessage);
-			printEmptyLine(displayMessage.size());
 		}
 		//The above is the top segment
 		printToUser(message);
@@ -45,8 +49,12 @@ void UI::readMessage(){
 		cout << endl << endl << MESSAGE_DEFAULT;
 		getline(cin, command);
 		//The above is the bottom segment
-		cout << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl;
-		message = logic.receiveCommand(command);
+		std::system("CLS");
+		checkOverdue(command);
+		//continue with the execution if the command is not check
+		if (command != "check"){
+			message = logic.receiveCommand(command);
+		}
 	}
 }
 
@@ -71,4 +79,39 @@ void UI::printSearchLine(int num){
 	for (int i = num; i < NUMBER_OF_SEARCH_LINES; i++){
 		cout << endl;
 	}
+}
+
+bool UI::checkOverdue(string command){
+	Logic logic;
+	string message=" ";
+	if (logic.displayOverdue().empty()){
+		return false;
+	}
+	while (!logic.displayOverdue().empty()){
+		string commandType=logic.getCommandType(command);
+		if (commandType == "add"){
+			printToUser(MESSAGE_CHECK_ADD);
+		}else if (commandType == "delete"){
+			printToUser(MESSAGE_CHECK_DELETE);
+		}else if (commandType == "edit"){
+			printToUser(MESSAGE_CHECK_EDIT);
+		}else if (commandType == "search"){
+			printToUser(MESSAGE_CHECK_SEARCH);
+		}else if (commandType == "archive"){
+			printToUser(MESSAGE_CHECK_ARCHIVE);
+		}else{
+			printToUser(MESSAGE_CHECK);
+		}
+		printVectorToUser(logic.displayOverdue());
+		//top
+		printToUser(message);
+		//middle
+		cout << endl;
+		printToUser(MESSAGE_DEFAULT_CHECK);
+		//bottom
+		getline(cin, command);
+		message = logic.receiveCommand(command);
+	}
+	std::system("CLS");
+	return true;
 }
--- Doooodle/Doooodle/UI.h
+++ Doooodle/Doooodle/UI.h
@@ -16,16 +16,24 @@ private:
 	Logic logic;
 	static const string MESSAGE_ADD;
 	static const string MESSAGE_DEFAULT;
+	static const string MESSAGE_DEFAULT_CHECK;
 	static const string MESSAGE_WELCOME;
 	static const string MESSAGE_TOPTEN;
 	static const string MESSAGE_FLOAT;
 	static const string MESSAGE_SEARCH;
+	static const string MESSAGE_CHECK;
+	static const string MESSAGE_CHECK_ADD;
+	static const string MESSAGE_CHECK_DELETE;
+	static const string MESSAGE_CHECK_SEARCH;
+	static const string MESSAGE_CHECK_EDIT;
+	static const string MESSAGE_CHECK_ARCHIVE;
 	static const int NUMBER_OF_TOTAL_LINES;
 	static const int NUMBER_OF_SEARCH_LINES;
 	void printToUser(string);
 	void printVectorToUser(vector<string>);
 	void printEmptyLine(int);
 	void printSearchLine(int);
+	bool checkOverdue(string);
 
 public:
 	UI(void);
