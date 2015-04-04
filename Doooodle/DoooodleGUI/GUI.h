ead18ae0296c25427a504e45ecdc468bcb2cc266
MitchTan <mitchell_tan@hotmail.com>
2015-04-04 11:36:11 +0800

    GUI edits

--- Doooodle/DoooodleGUI/GUI.h
+++ Doooodle/DoooodleGUI/GUI.h
@@ -25,7 +25,8 @@ namespace DoooodleGUI {
 	{
 	private:
 		Logic* logic;
-		Storage* storage;
+	private: System::Windows::Forms::TextBox^  textBox3;
+			 Storage* storage;
 
 	public:
 		GUI(void)
@@ -52,14 +53,14 @@ namespace DoooodleGUI {
 	private: System::Windows::Forms::TextBox^  textBox2;
 	private: System::Windows::Forms::TextBox^  textBox1;
 	protected:
-	private: System::Windows::Forms::DataGridView^  dataGridView1;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column1;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column2;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column3;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column4;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column5;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Column6;
-	private: System::Windows::Forms::ListView^  listView1;
+
+
+
+
+
+
+
+
 	private: System::Windows::Forms::PictureBox^  pictureBox1;
 	private: System::Windows::Forms::Label^  label1;
 	private: System::Windows::Forms::DateTimePicker^  dateTimePicker1;
@@ -79,25 +80,18 @@ namespace DoooodleGUI {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(GUI::typeid));
-			System::Windows::Forms::ListViewItem^  listViewItem1 = (gcnew System::Windows::Forms::ListViewItem(gcnew cli::array< System::String^  >(1) { L"" },
-				-1, System::Drawing::Color::Empty, System::Drawing::Color::Transparent, nullptr));
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
-			this->Column1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column3 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column4 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column5 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column6 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->listView1 = (gcnew System::Windows::Forms::ListView());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->dateTimePicker1 = (gcnew System::Windows::Forms::DateTimePicker());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
 			this->textBox1->Location = System::Drawing::Point(17, 418);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(756, 20);
@@ -105,65 +99,21 @@ namespace DoooodleGUI {
 			this->textBox1->TextChanged += gcnew System::EventHandler(this, &GUI::textBox1_TextChanged);
 			this->textBox1->Enter += gcnew System::EventHandler(this, &GUI::textBox1_TextChanged);
 			this->textBox1->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &GUI::textBox1_KeyDown);
-			this->dataGridView1->AllowUserToAddRows = false;
-			this->dataGridView1->AllowUserToDeleteRows = false;
-			this->dataGridView1->AllowUserToResizeColumns = false;
-			this->dataGridView1->AllowUserToResizeRows = false;
-			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(6) {
-				this->Column1, this->Column2,
-					this->Column3, this->Column4, this->Column5, this->Column6
-			});
-			this->dataGridView1->Location = System::Drawing::Point(17, 44);
-			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->ReadOnly = true;
-			this->dataGridView1->RowHeadersWidth = 10;
-			this->dataGridView1->Size = System::Drawing::Size(502, 327);
-			this->dataGridView1->TabIndex = 1;
-			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &GUI::dataGridView1_CellContentClick);
-			this->Column1->HeaderText = L"No.";
-			this->Column1->Name = L"Column1";
-			this->Column1->ReadOnly = true;
-			this->Column1->Width = 30;
-			this->Column2->HeaderText = L"Task Details";
-			this->Column2->Name = L"Column2";
-			this->Column2->ReadOnly = true;
-			this->Column2->Width = 260;
-			this->Column3->HeaderText = L"Date";
-			this->Column3->Name = L"Column3";
-			this->Column3->ReadOnly = true;
-			this->Column3->Width = 50;
-			this->Column4->HeaderText = L"Time";
-			this->Column4->Name = L"Column4";
-			this->Column4->ReadOnly = true;
-			this->Column4->Width = 50;
-			this->Column5->HeaderText = L"Date";
-			this->Column5->Name = L"Column5";
-			this->Column5->ReadOnly = true;
-			this->Column5->Width = 50;
-			this->Column6->HeaderText = L"Time";
-			this->Column6->Name = L"Column6";
-			this->Column6->ReadOnly = true;
-			this->Column6->Width = 50;
-			this->listView1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"listView1.BackgroundImage")));
-			this->listView1->GridLines = true;
-			this->listView1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ListViewItem^  >(1) { listViewItem1 });
-			this->listView1->Location = System::Drawing::Point(547, 57);
-			this->listView1->Name = L"listView1";
-			this->listView1->Size = System::Drawing::Size(226, 287);
-			this->listView1->TabIndex = 2;
-			this->listView1->UseCompatibleStateImageBehavior = false;
-			this->listView1->View = System::Windows::Forms::View::List;
-			this->listView1->SelectedIndexChanged += gcnew System::EventHandler(this, &GUI::listView1_SelectedIndexChanged_1);
+			// 
+			// pictureBox1
+			// 
 			this->pictureBox1->BackColor = System::Drawing::Color::Transparent;
 			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
 			this->pictureBox1->Location = System::Drawing::Point(560, 350);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(89, 62);
+			this->pictureBox1->Size = System::Drawing::Size(80, 62);
 			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
 			this->pictureBox1->TabIndex = 3;
 			this->pictureBox1->TabStop = false;
 			this->pictureBox1->Click += gcnew System::EventHandler(this, &GUI::pictureBox1_Click);
+			// 
+			// label1
+			// 
 			this->label1->AutoSize = true;
 			this->label1->BackColor = System::Drawing::Color::Transparent;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Kristen ITC", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
@@ -174,13 +124,19 @@ namespace DoooodleGUI {
 			this->label1->TabIndex = 4;
 			this->label1->Text = L"Floating Task";
 			this->label1->Click += gcnew System::EventHandler(this, &GUI::label1_Click);
+			// 
+			// dateTimePicker1
+			// 
 			this->dateTimePicker1->Location = System::Drawing::Point(17, 12);
 			this->dateTimePicker1->Name = L"dateTimePicker1";
 			this->dateTimePicker1->Size = System::Drawing::Size(200, 20);
 			this->dateTimePicker1->TabIndex = 5;
+			// 
+			// label2
+			// 
 			this->label2->AutoSize = true;
 			this->label2->BackColor = System::Drawing::Color::Transparent;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Gabriola", 14.25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label2->Font = (gcnew System::Drawing::Font(L"Gabriola", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label2->Location = System::Drawing::Point(258, 6);
 			this->label2->Name = L"label2";
@@ -188,29 +144,48 @@ namespace DoooodleGUI {
 			this->label2->TabIndex = 6;
 			this->label2->Text = L"Hi Jim!";
 			this->label2->Click += gcnew System::EventHandler(this, &GUI::label2_Click);
+			// 
+			// textBox2
+			// 
 			this->textBox2->BackColor = System::Drawing::Color::BurlyWood;
-			this->textBox2->Location = System::Drawing::Point(547, 57);
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox2->Location = System::Drawing::Point(552, 57);
 			this->textBox2->Multiline = true;
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->ReadOnly = true;
 			this->textBox2->Size = System::Drawing::Size(226, 287);
 			this->textBox2->TabIndex = 7;
 			this->textBox2->TextChanged += gcnew System::EventHandler(this, &GUI::textBox2_TextChanged);
+			// 
+			// textBox3
+			// 
+			this->textBox3->BackColor = System::Drawing::Color::Moccasin;
+			this->textBox3->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox3->Font = (gcnew System::Drawing::Font(L"Calibri", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox3->Location = System::Drawing::Point(17, 57);
+			this->textBox3->Multiline = true;
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->ReadOnly = true;
+			this->textBox3->Size = System::Drawing::Size(502, 319);
+			this->textBox3->TabIndex = 8;
+			// 
+			// GUI
+			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->ClientSize = System::Drawing::Size(790, 450);
+			this->Controls->Add(this->textBox3);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->dateTimePicker1);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->pictureBox1);
-			this->Controls->Add(this->listView1);
-			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->textBox1);
 			this->Name = L"GUI";
 			this->Text = L"GUI";
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
@@ -236,25 +211,23 @@ namespace DoooodleGUI {
 			Marshal::FreeHGlobal(IntPtr((void*)chars));
 		}
 
-		void addRowToDataGridView(String^& ID, String^& name, String^& Importance, String^& status, String^& Location, String^& startTime, String^& endTime){
-			dataGridView1->Rows->Add(ID, name, Importance, status, Location, startTime, endTime);
-		}
-
 		void clearUserInput(){
 			textBox1->Clear();
 		}
 
 		void clearGUI(){
-			dataGridView1->Rows->Clear();
-			dataGridView1->Refresh();
 			textBox2->Clear();
+			textBox3->Clear();
 		}
+
+		void processIndividualTask() {
+
+		}
+
 
 #pragma endregion
 	private: System::Void listView1_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
 	}
-private: System::Void dataGridView1_CellContentClick(System::Object^  sender, System::Windows::Forms::DataGridViewCellEventArgs^  e) {
-}
 private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
 }
 private: System::Void label2_Click(System::Object^  sender, System::EventArgs^  e) {
@@ -272,8 +245,17 @@ private: System::Void textBox1_KeyDown(System::Object^  sender, System::Windows:
 		logic->receiveCommand(input);
 		storage = logic->getStorage();
 		std::vector<std::string> floatingTask = storage->retrieveFloatingTask();
-		for (int i = 0; i < floatingTask.size(); i++) {
-			textBox2->Text = textBox2->Text + "\r\n" + (convertStdToManaged(floatingTask[i]));
+		std::vector<std::string> topTen = storage->retrieveTopTen();
+		if (floatingTask.size() > 0) {
+			textBox2->Text = convertStdToManaged(floatingTask[0]);
+			for (int i = 1; i<floatingTask.size(); i++) {
+				textBox2->Text = textBox2->Text + "\r\n" + (convertStdToManaged(floatingTask[i]));
+			}
+		}
+		textBox3->ForeColor = Color::DarkBlue;
+		textBox3->Text = "Below is the list of upcoming events in your calendar:";
+		for (int i = 0; i < topTen.size(); i++) {
+			textBox3->Text = textBox3->Text + "\r\n" + (convertStdToManaged(topTen[i]));
 		}
 		clearUserInput();
 	}
