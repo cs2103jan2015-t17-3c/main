307809b3c8a636e313bde961d0c4c25c11edb049
MitchTan <mitchell_tan@hotmail.com>
2015-04-05 09:45:54 +0800

    GUI and logic updates

--- Doooodle/DoooodleGUI/GUI.h
+++ Doooodle/DoooodleGUI/GUI.h
@@ -26,6 +26,9 @@ namespace DoooodleGUI {
 	private:
 		Logic* logic;
 	private: System::Windows::Forms::TextBox^  textBox3;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::TextBox^  textBox4;
+
 			 Storage* storage;
 
 	public:
@@ -64,7 +67,7 @@ namespace DoooodleGUI {
 	private: System::Windows::Forms::PictureBox^  pictureBox1;
 	private: System::Windows::Forms::Label^  label1;
 	private: System::Windows::Forms::DateTimePicker^  dateTimePicker1;
-	private: System::Windows::Forms::Label^  label2;
+
 
 	private:
 		/// <summary>
@@ -84,9 +87,10 @@ namespace DoooodleGUI {
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->dateTimePicker1 = (gcnew System::Windows::Forms::DateTimePicker());
-			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -116,11 +120,11 @@ namespace DoooodleGUI {
 			// 
 			this->label1->AutoSize = true;
 			this->label1->BackColor = System::Drawing::Color::Transparent;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Kristen ITC", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label1->Font = (gcnew System::Drawing::Font(L"Gabriola", 26.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(580, 21);
+			this->label1->Location = System::Drawing::Point(565, 29);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(170, 33);
+			this->label1->Size = System::Drawing::Size(171, 65);
 			this->label1->TabIndex = 4;
 			this->label1->Text = L"Floating Task";
 			this->label1->Click += gcnew System::EventHandler(this, &GUI::label1_Click);
@@ -132,44 +136,52 @@ namespace DoooodleGUI {
 			this->dateTimePicker1->Size = System::Drawing::Size(200, 20);
 			this->dateTimePicker1->TabIndex = 5;
 			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->BackColor = System::Drawing::Color::Transparent;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Gabriola", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(258, 6);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(57, 35);
-			this->label2->TabIndex = 6;
-			this->label2->Text = L"Hi Jim!";
-			this->label2->Click += gcnew System::EventHandler(this, &GUI::label2_Click);
-			// 
 			// textBox2
 			// 
 			this->textBox2->BackColor = System::Drawing::Color::BurlyWood;
 			this->textBox2->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->textBox2->Location = System::Drawing::Point(552, 57);
+			this->textBox2->Location = System::Drawing::Point(552, 80);
 			this->textBox2->Multiline = true;
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->ReadOnly = true;
-			this->textBox2->Size = System::Drawing::Size(226, 287);
+			this->textBox2->Size = System::Drawing::Size(226, 264);
 			this->textBox2->TabIndex = 7;
-			this->textBox2->TextChanged += gcnew System::EventHandler(this, &GUI::textBox2_TextChanged);
 			// 
 			// textBox3
 			// 
 			this->textBox3->BackColor = System::Drawing::Color::Moccasin;
 			this->textBox3->BorderStyle = System::Windows::Forms::BorderStyle::None;
-			this->textBox3->Font = (gcnew System::Drawing::Font(L"Calibri", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->textBox3->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->textBox3->Location = System::Drawing::Point(17, 57);
+			this->textBox3->Location = System::Drawing::Point(17, 80);
 			this->textBox3->Multiline = true;
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->ReadOnly = true;
-			this->textBox3->Size = System::Drawing::Size(502, 319);
+			this->textBox3->Size = System::Drawing::Size(502, 296);
 			this->textBox3->TabIndex = 8;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::Color::Transparent;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Gabriola", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(8, 35);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(140, 50);
+			this->label3->TabIndex = 9;
+			this->label3->Text = L" Welcome Jim!";
+			// 
+			// textBox4
+			// 
+			this->textBox4->BackColor = System::Drawing::SystemColors::ScrollBar;
+			this->textBox4->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox4->Location = System::Drawing::Point(17, 392);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->ReadOnly = true;
+			this->textBox4->Size = System::Drawing::Size(502, 13);
+			this->textBox4->TabIndex = 10;
 			// 
 			// GUI
 			// 
@@ -178,8 +190,9 @@ namespace DoooodleGUI {
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->ClientSize = System::Drawing::Size(790, 450);
 			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->label3);
 			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->label2);
 			this->Controls->Add(this->dateTimePicker1);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->pictureBox1);
@@ -244,11 +257,12 @@ private: System::Void textBox1_KeyDown(System::Object^  sender, System::Windows:
 		std::string cat;
 		std::vector<std::string> displayMessage;
 		MarshalString(userInput, input);
-		logic->receiveCommand(input);
+		std::string message = logic->receiveCommand(input);
 		storage = logic->getStorage();
 		std::vector<std::string> floatingTask = storage->retrieveFloatingTask();
 		std::vector<std::string> topTen = storage->retrieveTopTen();
 
+		label1->Text = "Floating Tasks [" + Convert::ToUInt32(logic->getFloatingSize()) + "]:";
 		if (floatingTask.size() > 0) {
 			textBox2->Text = convertStdToManaged(floatingTask[0]);
 			for (int i = 1; i<floatingTask.size(); i++) {
@@ -258,31 +272,37 @@ private: System::Void textBox1_KeyDown(System::Object^  sender, System::Windows:
 
 		if (logic->getCommandType(input) == "search") {
 			displayMessage = logic->displaySearchResults(input);
-			textBox3->Text = "Search Results [" + Convert::ToUInt32(displayMessage.size()) + "]:";
-			for (int i = 0; i < displayMessage.size(); i++) {
-				textBox3->Text = textBox3->Text + "\r\n" + (convertStdToManaged(displayMessage[i]));
+			label3->Text = "Search Results [" + Convert::ToUInt32(displayMessage.size()) + "]:";
+			if (displayMessage.size()>0) {
+				textBox3->Text = convertStdToManaged(displayMessage[0]);
+				for (int i = 1; i < displayMessage.size(); i++) {
+					textBox3->Text = textBox3->Text + "\r\n" + (convertStdToManaged(displayMessage[i]));
+				}
 			}
 		}
 		else if (logic->getCommandType(input) == "display"){
 			displayMessage = logic->displayCategoricalTask(input, cat);
-			textBox3->Text = "Below is the list of " + convertStdToManaged(cat) + " tasks [" + Convert::ToUInt32(displayMessage.size()) + "]:";
-			for (int i = 0; i < displayMessage.size(); i++) {
-				textBox3->Text = textBox3->Text + "\r\n" + (convertStdToManaged(displayMessage[i]));
+			label3->Text = "Below is the list of " + convertStdToManaged(cat) + " tasks [" + Convert::ToUInt32(displayMessage.size()) + "]:";
+			if (displayMessage.size()>0) {
+				textBox3->Text = convertStdToManaged(displayMessage[0]);
+				for (int i = 1; i < displayMessage.size(); i++) {
+					textBox3->Text = textBox3->Text + "\r\n" + (convertStdToManaged(displayMessage[i]));
+				}
 			}
 		}
 		else {
-			textBox3->Text = "Below is the list of upcoming events in your calendar [" + Convert::ToUInt32(logic->getNormalSize()) + "] [" + Convert::ToUInt32(logic->getNormalSize()) + "]:";
-			for (int i = 0; i < topTen.size(); i++) {
-				textBox3->Text = textBox3->Text + "\r\n" + (convertStdToManaged(topTen[i]));
+			label3->Text = "Below is the list of upcoming events in your calendar [" + Convert::ToUInt32(logic->getDeadlineSize()) + "] [" + Convert::ToUInt32(logic->getNormalSize()) + "]:";
+			if (topTen.size()>0) {
+				textBox3->Text = convertStdToManaged(topTen[0]);
+				for (int i = 1; i < topTen.size(); i++) {
+					textBox3->Text = textBox3->Text + "\r\n" + (convertStdToManaged(topTen[i]));
+				}
+
 			}
 		}
-
+		textBox4->Text = convertStdToManaged(message);
 		clearUserInput();
 	}
-}
-private: System::Void listView1_SelectedIndexChanged_1(System::Object^  sender, System::EventArgs^  e) {
-}
-private: System::Void textBox2_TextChanged(System::Object^  sender, System::EventArgs^  e) {
 }
 };
 }
